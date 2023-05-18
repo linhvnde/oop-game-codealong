5a8dd6dc89e552b1a67fed634e8e0b87153e05e2
linhvnde <linh.nh.tyty@gmail.com>
2023-05-18 23:35:10 +0200

    added moves (up,down) and scores

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 class Player {
   constructor() {
-    this.positionX = 40;
+    this.width = 10; //no unit here because we wanna manipulate them later
+    this.height = 10;
+    this.positionX = 50 - this.width / 2;
     this.positionY = 0;
-    this.width = 20; //no unit here because we wanna manipulate them later
-    this.height = 20;
+
     this.domElm = null; //store a ref. to the dom element of the player
 
     this.createDomElm(); //call the method here to shorten the code
@@ -23,8 +24,8 @@ class Player {
   }
   moveLeft() {
     if (this.positionX > 0) {
-      this.positionX -= 1;
-      console.log(`this.positionX ${this.positionX}`);
+      this.positionX -= 2;
+      //   console.log(`this.positionX ${this.positionX}`);
       this.domElm.style.bottom = this.positionY + "vh";
       this.domElm.style.left = this.positionX + "vw";
     }
@@ -32,8 +33,25 @@ class Player {
   moveRight() {
     const maxPositionX = 100 - this.width;
     if (this.positionX <= maxPositionX) {
-      this.positionX += 1;
-      console.log(`this.positionX ${this.positionX}`);
+      this.positionX += 2;
+      //   console.log(`this.positionX ${this.positionX}`);
+      this.domElm.style.bottom = this.positionY + "vh";
+      this.domElm.style.left = this.positionX + "vw";
+    }
+  }
+  moveUp() {
+    const maxPositionY = 100 - this.height;
+    if (this.positionY <= maxPositionY) {
+      this.positionY += 2;
+      //   console.log(`this.positionX ${this.positionX}`);
+      this.domElm.style.bottom = this.positionY + "vh";
+      this.domElm.style.left = this.positionX + "vw";
+    }
+  }
+  moveDown() {
+    if (this.positionY > 0) {
+      this.positionY -= 2;
+      //   console.log(`this.positionX ${this.positionX}`);
       this.domElm.style.bottom = this.positionY + "vh";
       this.domElm.style.left = this.positionX + "vw";
     }
@@ -45,7 +63,7 @@ class Obstacle {
     this.positionX = Math.floor(Math.random() * 100); //creat new obstacle that appear horizontal randomly
     this.positionY = 100;
     this.width = 10;
-    this.height = 20;
+    this.height = 10;
     this.domElm = null;
     this.createDomElm();
   }
@@ -63,10 +81,10 @@ class Obstacle {
     parentElm.appendChild(this.domElm);
   }
   moveDown() {
-    this.positionY -= 5;
+    this.positionY -= 1;
     this.domElm.style.bottom = this.positionY + "vh";
 
-    console.log(`new position of obstacle.... ${this.positionY}`);
+    // console.log(`new position of obstacle.... ${this.positionY}`);
   }
 }
 const player = new Player();
@@ -77,21 +95,39 @@ const obstaclesArr = []; // will store instances of the class Obstacle
 setInterval(() => {
   const newObstacle = new Obstacle();
   obstaclesArr.push(newObstacle);
+  let scores = obstaclesArr.length;
+  const displayElm = document.getElementById("score-display");
+  displayElm.innerText = scores;
 }, 3000);
 
-// Move all obstacles
+//// Update obstacles
 setInterval(() => {
   obstaclesArr.forEach((obstacle) => {
+    //move obstacles
     obstacle.moveDown();
+    //detect collision
+    if (
+      obstacle.positionX < player.positionX + player.width &&
+      obstacle.positionX + obstacle.width > player.positionX &&
+      obstacle.positionY < player.positionY + player.height &&
+      obstacle.height + obstacle.positionY > player.positionY
+    ) {
+      console.log("game over my fren");
+      location.href = "./gameover.html";
+    }
   });
-}, 60);
+}, 100);
 
 // attach event listeners...
 document.addEventListener("keydown", (event) => {
-  console.log(`user press ${event.code}`);
+  //   console.log(`user press ${event.code}`);
   if (event.code === "ArrowLeft") {
     player.moveLeft();
-  } else {
+  } else if (event.code === "ArrowRight") {
     player.moveRight();
+  } else if (event.code === "ArrowUp") {
+    player.moveUp();
+  } else if (event.code === "ArrowDown") {
+    player.moveDown();
   }
 });
